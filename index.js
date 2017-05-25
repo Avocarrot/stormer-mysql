151f26de8be09931ab6d87834653491bcd67deac
Dmtrs Mengidis <tydeas.dr@gmail.com>
2017-05-25 17:26:46 +0300

    DRAFT

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MySqlStore = require('./lib/store');
+const MySqlCachableStore = require('./lib/cachableStore');
 const MySqlCondition = require('./lib/condition');
 const MockStore = require('./mock');
 
-module.exports = { MySqlStore, MySqlCondition, MockStore };
+module.exports = { MySqlStore, MySqlCondition, MockStore, MySqlCachableStore };
